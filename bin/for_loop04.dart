295648289512ed37020a66e5187cc068861d7c62
ogabek <norpulatovogabek42@gmail.com>
2022-07-20 09:25:40 +0500

    task4

--- bin/for_loop04.dart
+++ bin/for_loop04.dart
@@ -1,4 +1,10 @@
 int func(List<int> numbers) {
+  int y = 0;
+  for (int q = 0; q < numbers.length; q++) {
+    if (numbers[q] % 2 == 0) {
+      y += numbers[q];
+    }
+  }
 /*
    Given the list of numbers, return the sum the even numbers in the list
 Args:
@@ -6,7 +12,15 @@ Args:
 Return: 
   Int: answer
 */
-  return 0;
+  return y;
 }
 
-void main() {}
+void main() {
+  print(func([
+    23,
+    3434,
+    3,
+    2,
+    1,
+  ]));
+}
