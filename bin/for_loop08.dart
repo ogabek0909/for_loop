69c1b069b97735f7f3b6d4e6bd1e52e9f3933cea
ogabek <norpulatovogabek42@gmail.com>
2022-07-20 09:56:46 +0500

    task8

--- bin/for_loop08.dart
+++ bin/for_loop08.dart
@@ -1,4 +1,10 @@
 int func(List<int> numbers, int k, int n) {
+  int y = 0;
+  for (int q = k; q < n; q++) {
+    if (numbers[q].isOdd) {
+      y += numbers[q];
+    }
+  }
 /*
    Given the list of numbers, return the sum of the odd numbers between k and n in the list
 Args:
@@ -8,7 +14,9 @@ Args:
 Return: 
   Int: answer
 */
-  return 0;
+  return y;
 }
 
-void main() {}
+void main() {
+  print(func([3, 4, 52, 55, 56], 0, 4));
+}
